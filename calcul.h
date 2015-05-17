17ae94bfe779eab212a22202ec7955c760b9d346
AxelDelsol63 <axel.delsol@laposte.net>
2015-05-17 09:58:51 +0200

    Ajout des fonctions de manipulation de la solution, ca peut servir

--- calcul.h
+++ calcul.h
@@ -17,7 +17,7 @@ pos_t compute_position(graph_t* G,int idm,double t);
 /*Calcul la position de l'intercepteur a partir d'un angle alpha, d'une position initiale et d'une date t*/
 pos_t compute_pos_intercep(graph_t* G, double alpha, pos_t pos, int t);
 
-/*Calcul le temps requis pour intercepter le mobile idm A PARTIR de la date t */
+/*Calcul le temps requis pour intercepter le mobile idm A PARTIR de la date t  et de la position pos*/
 double compute_interception(graph_t* G, pos_t* pos, int idm,int t, double* alpha);
 
 /*Calcul la solution  de l'équation a*cos(x)+b*sin(x) = c pour x inconnu*/
