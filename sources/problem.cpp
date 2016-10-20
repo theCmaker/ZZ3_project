729df44fe060210f119b319ccc36eb4e7f7e71f1
auperes <auperes@poste.isima.fr>
2016-10-20 17:56:18 +0200

    Ajout de la classe Problem

---
+++ sources/problem.cpp
@@ -0,0 +1,9 @@
+#include "problem.hpp"
+
+Problem::Problem() {}
+
+Problem::Problem(std::string) {}
+
+Problem::Problem(Problem &) {}
+
+Problem::~Problem() {}
---
+++ sources/problem.hpp
@@ -0,0 +1,25 @@
+#ifndef __PROBLEM_HPP__
+#define __PROBLEM_HPP__
+
+#include <vector>
+#include <iostream>
+#include "interceptor.hpp"
+#include "mobile.hpp"
+
+class Problem
+{
+	private:
+		std::vector<Interceptor> 	_interceptors;
+		std::vector<Mobile> 		_mobiles;
+		//std::vector<Depot>		_depots;
+	
+	public:
+		Problem();
+		Problem(std::string);
+		Problem(Problem &);
+		~Problem();
+		
+		
+};
+
+#endif
