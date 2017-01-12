9e5cb98fa3438c3e7c2369e7afb41cf88887e55b
auperes <auperes@poste.isima.fr>
2017-01-12 18:06:56 +0100

    sources/move.cpp
    Addition of the class Move and its derivated classes. Implementation of InsertMove is almost done.

--- sources/move.cpp
+++ sources/move.cpp
@@ -9,7 +9,7 @@ Move::~Move() {}
 
 // implementation of InsertMove
 
-InsertMove::InsertMove(Problem & p, const Mobile & m_in, int m_prev, const Interceptor & i) : Move(p), _mobile_in(m_in), _mobile_prev(m_prev), _interceptor(i) {}
+InsertMove::InsertMove(Problem & p, const Mobile & m_in, int m_prev, const Interceptor & i) : Move(p), _mobile_in(m_in), _mobile_prev(m_prev), _interceptor(i), _interception_date(-1) {}
 InsertMove::~InsertMove() {}
 		
 bool InsertMove::scan(const Solution & solution)
@@ -23,7 +23,7 @@ bool InsertMove::scan(const Solution & solution)
 	
 	if(_mobile_prev >= 0)
 	{
-		// compute time of interception of mobile_prev (mobile before the insertion)
+		// computes time of interception of mobile_prev (mobile before the insertion)
 		interception_time = solution.mobile(_mobile_prev)._date;
 		
 		// iterator of the mobile next mobile_prev
@@ -35,15 +35,16 @@ bool InsertMove::scan(const Solution & solution)
 		interceptor_position = _p.mobiles()[_mobile_prev].position(interception_time);
 	}
 	
-	// sum with the interception time of the mobile inserted 
+	// sums with the interception time of the mobile inserted 
 	interception_time += _interceptor.computeInterception(interceptor_position,
 										 				  _mobile_in,
 														  interception_time,
 														  alpha);
 	interceptor_position = _p.mobiles()[_mobile_in.id()].position(interception_time);
+	_interception_date = interception_time;
 														  
-	// check if there is no problem in the route after the insertion
-	// and compute the interception time for each following mobile
+	// checks if there is no problem in the route after the insertion
+	// and computes the interception time for each following mobile
 	while(mobile_it != solution.end(_interceptor) && std::isfinite(interception_time))
 	{
 		interception_time += _interceptor.computeInterception(interceptor_position,
@@ -54,7 +55,7 @@ bool InsertMove::scan(const Solution & solution)
 		++mobile_it;
 	}
 									 
-	// compare time of interception between the update route and the worse route (in time)
+	// compares time of interception between the update route and the worse route (in time)
 	if (!std::isfinite(interception_time) || solution.worst_interception_time() < interception_time)
 	{
 		improved = false;
@@ -63,7 +64,20 @@ bool InsertMove::scan(const Solution & solution)
 	return improved;
 }
 
-void InsertMove::commit(Solution &)
+void InsertMove::commit(Solution & solution)
 {
-
+	// insertion ahead or creation of a route
+	if(_mobile_prev < 0)
+	{
+		// solution.prepend(...) TODO
+	}
+	// if insertion at end
+	else if(solution.mobile(_mobile_prev)._next < 0)
+	{
+		solution.append(_interceptor, _mobile_in, _interception_date);
+	}
+	else
+	{
+		// solution.insertAfter(...) TODO
+	}
 }
