351d8b71b9a22871229c870ae9f90dc14cb4635c
auperes <auperes@poste.isima.fr>
2017-01-26 18:19:31 +0100

    Doxygen documentation added for the classes Move and InsertMove

--- sources/move.hpp
+++ sources/move.hpp
@@ -1,38 +1,87 @@
+/**
+ * @file move.hpp
+ * @brief Declaration of an abstract class Move and its implementations (Insert, Delete, Cross, Replace).
+ */
+
 #ifndef __MOVE_HPP__
 #define __MOVE_HPP__
 
 #include "solution.hpp"
 
+/**
+ * @brief Abstract class to make a movement in a route.
+ */
 class Move
 {
 	protected:
-		Problem & _p;
+		Problem & _p; ///< Routing problem
 		
 	public:
-		Move(Problem &);
+		/**
+		 * @brief Constructor.
+		 * @param p The routing problem the move refers to.
+		 */
+		Move(Problem & p);
+		/**
+		 * @brief Destructor.
+		 */
 		virtual ~Move();
 		
-		virtual bool scan(const Solution &) = 0;
-		virtual void commit(Solution &) = 0;
+		/**
+		 * @brief Scans if the movement improves the solution.
+		 * @param solution The solution where the movement will be scanned.
+		 */
+		virtual bool scan(const Solution & solution) = 0;
+		
+		/**
+		 * @brief Commits the movement after a scan.
+		 * @param solution The solution where the movement will be done.
+		 */
+		virtual void commit(Solution & solution) = 0;
 };
 
+/**
+ * @brief Movement to insert a mobile in a given route.
+ */
 class InsertMove : public Move
 {
 	private:
-		const Mobile & 			_mobile_in;		// mobile to insert
-		int						_mobile_prev;	// insertion of the mobile will be after this one
-		const Interceptor & 	_interceptor;	// interceptor for the wanted route
-		Time					_interception_date; // time of interception of _mobile_in
+		const Mobile & 			_mobile_in;		///< Mobile to insert
+		int						_mobile_prev;	///< Insertion of the mobile will be after this one
+		const Interceptor & 	_interceptor;	///< Interceptor for the wanted route
+		Time					_interception_date; ///< Interception time of _mobile_in
 		
 	public:
-		InsertMove(Problem &, const Mobile &, int, const Interceptor &);
+		/**
+		 * @brief Constructor.
+		 * @param p The routing problem the move refers to.
+		 * @param m_in The mobile to insert in the route.
+		 * @param m_prev The mobile which will be before the mobile to insert.
+		 * @param i The interceptor of the route.
+		 */
+		InsertMove(Problem & p, const Mobile & m_in, int m_prev, const Interceptor & i);
+		/**
+		 * @brief Destructor.
+		 */
 		virtual ~InsertMove();
 		
+		/**
+		 * @brief Scans if the insertion of the mobile improves the solution.
+		 * @param solution The solution where the insertion will be scanned.
+		 */
 		bool scan(const Solution &);
+		
+		/**
+		 * @brief Commits the insertion after a scan.
+		 * @param solution The solution where the insertion will be done.
+		 */
 		virtual void commit(Solution &);
 };
 
-class DeleteMove : public Move
+/**
+ * @brief Movement to delete a mobile in a given route.
+ */
+class DeleteMove : public Move //TODO not implemented
 {
 	private:
 		
@@ -44,7 +93,10 @@ class DeleteMove : public Move
 		virtual void commit(Solution &);
 };
 
-class CrossMove : public Move
+/**
+ * @brief Movement to cross two routes.
+ */
+class CrossMove : public Move //TODO not implemented
 {
 	private:
 		
@@ -56,7 +108,10 @@ class CrossMove : public Move
 		virtual void commit(Solution &);
 };
 
-class ReplaceMove : public Move
+/**
+ * @brief Movement to replace a mobile by another in a given route.
+ */
+class ReplaceMove : public Move //TODO not implemented
 {
 	private:
 		
