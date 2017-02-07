cdfaabfa630526ea2cfb40130ffa05263d5745f9
theCmaker <pierre-loup.pissavy94@orange.fr>
2017-02-07 21:28:58 +0100

    MoveExtract debug: Case "no route" geared

--- sources/move.cxx
+++ sources/move.cxx
@@ -114,48 +114,51 @@ MoveExtract<Policy>::~MoveExtract() {}
 template <typename Policy>
 bool MoveExtract<Policy>::scan(const Solution & solution)
 {
-	const Problem & _p = solution.problem();
-	//TODO: comment it!
-	Policy::reset();
 	bool improved = false;
-	const Solution::MobileNode * previousNode;
-	const Interceptor & interceptor = solution.worstRoute();
-	Time interception_date;
-	Location interceptor_position;
-	_best_interception_date = solution.lastInterceptionTime(interceptor); // Worst time is the time for the worst route
+	if (solution.first() >= 0) {
+		// If there is at least one route.
+		const Problem & _p = solution.problem();
+		//TODO: comment it!
+		Policy::reset();
+		const Solution::MobileNode * previousNode;
+		const Interceptor & interceptor = solution.worstRoute();
+		Time interception_date;
+		Location interceptor_position;
+		_best_interception_date = solution.lastInterceptionTime(interceptor); // Worst time is the time for the worst route
 
-	// Mobile to be extracted
-	Solution::const_iterator position_it = solution.begin(interceptor);
+		// Mobile to be extracted
+		Solution::const_iterator position_it = solution.begin(interceptor);
 
-	// Find an extraction position
-	while (position_it != solution.end(interceptor) && Policy::keepOn()) {
-		if (position_it->_prev < 0) {
-			// First of route
-			interception_date = 0.;
-			interceptor_position = interceptor.position();
-		} else {
-			// Into the route
-			previousNode = &(solution.mobile((unsigned) position_it->_prev));
-
-			interception_date = previousNode->_date;
-			interceptor_position = previousNode->_mobile.position(interception_date);
-		}
-
-		// Get the time to catch the rest of the route (same order), if any.
-		if (position_it->_next >= 0) {
-			interception_date += solution.evaluate(interceptor_position, interception_date, interceptor, _p.mobiles()[position_it->_next], solution.lastOfRoute(interceptor)._mobile);
-		}
-		if (std::isfinite(interception_date) && Policy::update(interception_date,_best_interception_date)) {
-			// Update results
-			_best_mobile_candidate = &(position_it->_mobile);
-			if (position_it->_next >= 0) {
-				_best_recompute_from = &(solution.mobile(position_it->_next)._mobile);
+		// Find an extraction position
+		while (position_it != solution.end(interceptor) && Policy::keepOn()) {
+			if (position_it->_prev < 0) {
+				// First of route
+				interception_date = 0.;
+				interceptor_position = interceptor.position();
 			} else {
-				_best_recompute_from = nullptr;
+				// Into the route
+				previousNode = &(solution.mobile((unsigned) position_it->_prev));
+
+				interception_date = previousNode->_date;
+				interceptor_position = previousNode->_mobile.position(interception_date);
 			}
-			improved = true;
+
+			// Get the time to catch the rest of the route (same order), if any.
+			if (position_it->_next >= 0) {
+				interception_date += solution.evaluate(interceptor_position, interception_date, interceptor, _p.mobiles()[position_it->_next], solution.lastOfRoute(interceptor)._mobile);
+			}
+			if (std::isfinite(interception_date) && Policy::update(interception_date,_best_interception_date)) {
+				// Update results
+				_best_mobile_candidate = &(position_it->_mobile);
+				if (position_it->_next >= 0) {
+					_best_recompute_from = &(solution.mobile(position_it->_next)._mobile);
+				} else {
+					_best_recompute_from = nullptr;
+				}
+				improved = true;
+			}
+			++position_it;
 		}
-		++position_it;
 	}
 	return improved;
 }
