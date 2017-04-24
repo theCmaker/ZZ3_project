1f9b5e45d4b48f2c4be8f769ef5fb8b44a0f9bd9
theCmaker <pierre-loup.pissavy94@orange.fr>
2017-04-25 00:10:56 +0200

    Updated benchmark launcher for larger benchmarking

--- sources/VND_benchmark/main.cpp
+++ sources/VND_benchmark/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <VND_benchmark/VNDBenchmark.hpp>
 int main(int argc, const char * argv[]) {
-	//Run the benchmark for 50 instances and 2000 variants
-	VNDBenchmark<50,2000> benchmark;
-	benchmark();
+	//Run the benchmark for 50 instances and 200 variants
+	VNDBenchmark<50u,2000u> benchmark;
 	for (auto sol : benchmark()) {
 		for (auto id : sol)
 			std::cout << id << " ";
